8d2b43bb14a89ed8b194597a8e3e4a787ac3956e
Salimou FOFANA <f.salimou@hotmail.fr>
2018-07-28 20:39:35 +0200

    Chapitre sécurité et évènement

--- src/OC/PlatformBundle/Antispam/OCAntispam.php
+++ src/OC/PlatformBundle/Antispam/OCAntispam.php
@@ -8,11 +8,15 @@ class OCAntispam
     private $locale;
     private $minLentgh;
 
-    public function __construct(\Swift_Mailer $mailer, $locale, $minLentgh){
+    public function __construct(\Swift_Mailer $mailer, $minLentgh){
         $this->mailer = $mailer;
-        $this->locale = $locale;
         $this->minLentgh = (int) $minLentgh;
-     }
+    }
+
+    public function setLocale($locale)
+    {
+        $this->locale = $locale;
+    }
 
     public function isSpam($text){
         return strlen($text) < $this->minLentgh;
